5a6fc237bf9f1766219a49d48af8d02b63a8ccce
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2017-01-19 20:16:06 +0100

    implement deserializing of son

--- app/actors/CheckersSocketActor.scala
+++ app/actors/CheckersSocketActor.scala
@@ -1,11 +1,18 @@
 package actors
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import de.htwg.se.checkers.controller.RegisterUI
+import de.htwg.se.checkers.controller.{DeregisterUI, RegisterUI}
 import de.htwg.se.checkers.model._
 import de.htwg.se.checkers.model.api.Coord
 import de.htwg.se.checkers.model.enumeration.Colour
-import play.api.libs.json.{JsObject, JsValue, Json, Writes}
+import play.api.libs.json._
+import akka.pattern.ask
+import de.htwg.se.checkers.controller.command._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 /**
   * Created by steffen on 14/01/2017.
@@ -17,6 +24,8 @@ object CheckersSocketActor {
 class CheckersSocketActor(val wsOut: ActorRef, val checkersController: ActorRef) extends Actor with ActorLogging {
 
   checkersController ! RegisterUI
+  implicit val timeout = akka.util.Timeout(5, TimeUnit.SECONDS)
+
 
   override def preStart: Unit = {
     log.info("Starting")
@@ -33,13 +42,23 @@ class CheckersSocketActor(val wsOut: ActorRef, val checkersController: ActorRef)
     case update: GameState => wsOut ! Json.toJson(transform(update))
 
     // message from websocket
-    case json: JsValue => json match {
+    case json: JsValue => transform(json) match {
 
+      case query: Query =>
+        checkersController ? query.command onSuccess {
+          case currentPlayer: Colour.Value => wsOut ! Json.toJson(currentPlayer)
+          case possibleMoves: Moves => wsOut ! Json.toJson(possibleMoves)
+          case possiblePieces: Origins => wsOut ! Json.toJson(possiblePieces)
+          case possibleTargets: Targets => wsOut ! Json.toJson(possibleTargets)
+          case update: GameState => wsOut ! Json.toJson(transform(update))
+        }
 
-      // TODO: distinguish json messages
-      case unknownMessage@_ =>
-        log.info(s"unknown message: $unknownMessage")
-        wsOut ! Json.toJson(unknownMessage)
+      case execute: Execute =>
+        checkersController ! execute.command
+
+      case default =>
+        log.info("Json from Websocket not recognised")
+        wsOut ! json
     }
 
     case default@_ => log.info(s"unkown message: $default from " + sender())
@@ -47,8 +66,60 @@ class CheckersSocketActor(val wsOut: ActorRef, val checkersController: ActorRef)
 
 
   override def postStop: Unit = {
-    // TODO:
-    // someResource.close()
+    checkersController ! DeregisterUI
+  }
+
+
+  def transformQuery(query: String, json: JsValue): Any = {
+    query.toLowerCase match {
+      case "getmoves" => Query(GetMoves)
+      case "getcurrentplayer" => Query(GetCurrentPlayer)
+      case "getpossiblepieces" => Query(GetPossiblePieces)
+      case "gamestatus" => Query(GameStatus)
+      case "getpossibletargets" =>
+        val x: Int = (json \ "x").validate[Int].getOrElse(-1)
+        val y: Int = (json \ "y").validate[Int].getOrElse(-1)
+
+        if (x < 0 || y < 0) json // error
+        else Query(GetPossibleTargets(x, y))
+
+      case default => json
+    }
+
+  }
+
+  def transformExecute(execute: String, json: JsValue): Any = {
+    execute.toLowerCase match {
+      case "quitgame" => Execute(QuitGame)
+      case "newgame" => Execute(NewGame)
+      case "setpiece" =>
+        val x1: Int = (json \ "x1").validate[Int].getOrElse(-1)
+        val y1: Int = (json \ "y1").validate[Int].getOrElse(-1)
+        val x2: Int = (json \ "x2").validate[Int].getOrElse(-1)
+        val y2: Int = (json \ "y2").validate[Int].getOrElse(-1)
+
+        if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0) json // error
+        else Execute(SetPiece((x1, y1), (x2, y2)))
+
+      case default => json
+    }
+  }
+
+  def transform(json: JsValue): Any = {
+    val execute: String = (json \ "execute").validate[String].getOrElse("")
+    val query: String = (json \ "query").validate[String].getOrElse("")
+
+
+    if (!execute.isEmpty && !query.isEmpty) {
+      log.info("both parameter are not allowed")
+      json
+    } else if (!execute.isEmpty) {
+      transformExecute(execute, json)
+    } else if (!query.isEmpty) {
+      transformQuery(query, json)
+    } else {
+      json
+    }
   }
 
 
