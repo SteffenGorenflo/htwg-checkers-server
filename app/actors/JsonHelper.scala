43a506bdd27481930f40a1101dc32d4478cc4378
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2017-01-19 20:15:44 +0100

    create wrapper for query and execute

--- app/actors/JsonHelper.scala
+++ app/actors/JsonHelper.scala
@@ -8,3 +8,5 @@ import de.htwg.se.checkers.model.enumeration.Colour
   * Created by steffen on 16/01/2017.
   */
 case class GameStateJson(vector: Seq[(Coord, Piece)], currentPlayer: Colour.Value)
+case class Query(command: Any) {}
+case class Execute(command: Any) {}
